b9997a4969853753cb1149032f0b71af5ff7bc70
Paulo <paulsix02@gmail.com>
2026-04-05 03:59:32 -0300

    feat: saga com aprovar/rejeitar e auth sycn com fila + authconsumer)

--- services/auth-service/src/main/java/com/dac/auth/config/RabbitMQConfig.java
+++ services/auth-service/src/main/java/com/dac/auth/config/RabbitMQConfig.java
@@ -1,6 +1,6 @@
 package com.dac.auth.config;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -13,10 +13,49 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
     public static final String FILA_RESET = "saga.reset";
+    public static final String AUTH_EXCHANGE = "auth.exchange";
+    public static final String FILA_AUTH_CRIAR = "auth.criar.queue";
+    public static final String FILA_AUTH_ATUALIZAR = "auth.atualizar.queue";
+    public static final String FILA_AUTH_REMOVER = "auth.remover.queue";
 
     @Bean
     public Queue filaReset() {
         return new Queue(FILA_RESET, true);
+    }
+
+    @Bean
+    public TopicExchange authExchange() {
+        return new TopicExchange(AUTH_EXCHANGE);
+    }
+
+    @Bean
+    public Queue filaAuthCriar() {
+        return new Queue(FILA_AUTH_CRIAR, true);
+    }
+
+    @Bean
+    public Queue filaAuthAtualizar() {
+        return new Queue(FILA_AUTH_ATUALIZAR, true);
+    }
+
+    @Bean
+    public Queue filaAuthRemover() {
+        return new Queue(FILA_AUTH_REMOVER, true);
+    }
+
+    @Bean
+    public Binding bindingCriar(Queue filaAuthCriar, TopicExchange authExchange) {
+        return BindingBuilder.bind(filaAuthCriar).to(authExchange).with("auth.criar");
+    }
+
+    @Bean
+    public Binding bindingAtualizar(Queue filaAuthAtualizar, TopicExchange authExchange) {
+        return BindingBuilder.bind(filaAuthAtualizar).to(authExchange).with("auth.atualizar");
+    }
+
+    @Bean
+    public Binding bindingRemover(Queue filaAuthRemover, TopicExchange authExchange) {
+        return BindingBuilder.bind(filaAuthRemover).to(authExchange).with("auth.remover");
     }
 
     @Bean
