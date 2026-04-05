55033c201f7059f53e93d9a7e1705285f4629eaf
Felyppe Silva <felyppe1201@gmail.com>
2026-04-05 00:43:46 -0300

    fix do conta, graças a Deus

--- services/conta-service/src/main/java/com/dac/conta/config/DataSourceConfig.java
+++ services/conta-service/src/main/java/com/dac/conta/config/DataSourceConfig.java
@@ -1,25 +1,70 @@
 package com.dac.conta.config;
 
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+    basePackages = "com.dac.conta.repository",
+    entityManagerFactoryRef = "entityManagerFactory",
+    transactionManagerRef = "transactionManager"
+)
 public class DataSourceConfig {
 
     @Bean
-    @Primary // Define este como o banco principal (CUD)
+    @Primary
     @ConfigurationProperties(prefix = "spring.datasource")
-    DataSource mainDataSource() {
+    public DataSource mainDataSource() {
         return DataSourceBuilder.create().build();
     }
 
-    @Bean // Define o banco de leitura (R)
+    @Bean
     @ConfigurationProperties(prefix = "spring.datasource-read")
-    DataSource readDataSource() {
+    public DataSource readDataSource() {
         return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            @Qualifier("mainDataSource") DataSource mainDataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(mainDataSource);
+        em.setPackagesToScan("com.dac.conta.entity");
+        em.setPersistenceUnitName("cud");
+
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setGenerateDdl(true);
+        em.setJpaVendorAdapter(adapter);
+
+        Properties props = new Properties();
+        props.setProperty("hibernate.hbm2ddl.auto", "update");
+        props.setProperty("hibernate.format_sql", "true");
+        props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        em.setJpaProperties(props);
+
+        return em;
+    }
+
+    @Bean
+    @Primary
+    public PlatformTransactionManager transactionManager(
+            @Qualifier("entityManagerFactory") EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
     }
 }
